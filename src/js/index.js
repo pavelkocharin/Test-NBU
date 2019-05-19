1ab2ca2291f6afa7a24d918d6459e6e04b468def
pavelkocharin <pavel.kocharin@gmail.com>
2019-05-20 02:54:18 +0500

    last edit

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,2 @@
-// import './range.js'
-// import './modal-saved.js'
-// import './registration.js'
-// import './mobile-menu.js'
-// import './login.js'
-// import './fogot-pass.js'
 import './play-video.js'
 import './show-image.js'
